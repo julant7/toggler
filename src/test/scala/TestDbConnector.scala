163035f45668f4ab53e394046331122ed3e365a1
Julia <yulia.antonova225@gmail.com>
2026-05-10 21:53:24 +0300

    feat: adding create

--- src/test/scala/TestDbConnector.scala
+++ src/test/scala/TestDbConnector.scala
@@ -1,13 +1,17 @@
 import cats.implicits.*
 import doobie.Transactor
+import entity.FeatureFlag
 import services.Connector
-import zio.{Task, ZLayer}
+import services.FeatureServiceImpl.createTable
+import zio.{Task, ZIO, ZLayer}
 import zio.interop.catz.*
+import doobie.*
+import doobie.implicits.*
 
 class TestDbConnector(transactor: Transactor[Task]) extends Connector(transactor)
 
 object TestDbConnector {
-  val layer: ZLayer[Any, Throwable, TestDbConnector] = ZLayer{
+  val layer: ZLayer[Nothing, Any, Option[Any]] = ZLayer{
     (for {
       config <- TestDbConfig.refFromFile()
     } yield TestDbConnector(Transactor.fromDriverManager[Task](
@@ -16,6 +20,21 @@ object TestDbConnector {
       user = config.user,
       password = config.password,
       logHandler = None
-    ))).mapError(err => throw new RuntimeException("Error while connecting to database"))
+    ))).forEachZIO(conn => createTable(conn.transactor))
+  }
+
+  def createTable: doobie.ConnectionIO[Int] =
+    sql"""|CREATE TABLE IF NOT EXISTS FLAGS(
+          |flag_id SERIAL PRIMARY KEY NOT NULL,
+          |key VARCHAR NOT NULL UNIQUE NOT NULL,
+          |rules JSON NOT NULL,
+          |created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+          |updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+          |is_deleted BOOLEAN DEFAULT FALSE NOT NULL
+          |)""".stripMargin.update.run
+
+
+  def createTable(xa: Transactor[Task]): zio. ZIO[Nothing, Any, Any] = {
+    createTable.transact(xa)
   }
 }
